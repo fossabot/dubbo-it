ca6fab5ca8568a9e13d514df69e4b69a9ea334a4
Frederick-S <mao_xiaodan@hotmail.com>
2019-03-10 20:53:34 +0800

    Add modal

--- web/src/components/Navigation.tsx
+++ web/src/components/Navigation.tsx
@@ -1,24 +1,32 @@
-import { Layout, Menu } from 'antd'
+import { Button, Layout, Modal } from 'antd'
 import React, { Component } from 'react'
 
 const { Header } = Layout
 
 class Navigation extends Component {
+  state = { isModalVisible: false }
+
   public render(): React.ReactNode {
     return (
       <Header className="header" tagName="header">
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
+        <Button type="primary" icon="plus-circle" onClick={this.showModal}>New</Button>
+        <Modal title="Basic Modal" visible={this.state.isModalVisible} onCancel={this.handleModalCancel} footer={null}>
+          <p>Hello</p>
+        </Modal>
       </Header>
     )
+  }
+
+  private showModal = () => {
+    this.setState({
+      isModalVisible: true
+    })
+  }
+
+  private handleModalCancel = (e: any) => {
+    this.setState({
+      isModalVisible: false
+    })
   }
 }
 
