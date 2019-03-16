fd6ee230bacc5f2c2ac525d014408df4c4485e04
Frederick-S <mao_xiaodan@hotmail.com>
2019-03-16 11:39:46 +0800

    Validate port

--- web/src/components/RegistryForm.tsx
+++ web/src/components/RegistryForm.tsx
@@ -17,7 +17,10 @@ class RegistryForm extends Component<FormComponentProps> {
         </Form.Item>
         <Form.Item label="Port">
           {getFieldDecorator('Port', {
-            rules: [{ required: true, message: 'Port is required' }],
+            rules: [
+              { pattern: /^()([1-9]|[1-5]?[0-9]{2,4}|6[1-4][0-9]{3}|65[1-4][0-9]{2}|655[1-2][0-9]|6553[1-5])$/, message: 'Invalid port' },
+              { required: true, message: 'Port is required' },
+            ],
           })(
             <Input />,
           )}
@@ -26,7 +29,7 @@ class RegistryForm extends Component<FormComponentProps> {
           <Input />
         </Form.Item>
         <Form.Item label="Password">
-          <Input />
+          <Input type="password" />
         </Form.Item>
       </Form>
     )
